992bba052c578ef5d6167001511cdf89e98c9139
Pyk3i <Pyk3i@users.noreply.github.com>
2024-04-17 21:00:01 -0300

    Mas ejercicios

--- guia4.hs
+++ guia4.hs
@@ -69,22 +69,75 @@ f1 n
     |n == 0 = 1
     |otherwise = 2^n + f1(n-1)
 
-f2:: Int -> Int -> Int
+f2:: Int -> Float -> Float
 f2 n q
     |n == 1 = q
     |otherwise = q^n + f2 (n-1) q
 
-{-- f3:: Int -> Int -> Int
+f3:: Int -> Float -> Float
 f3 n q
-    |--}
+    |n == 1 = q^2 + q
+    |otherwise = q^(2*n) + q^(2*n - 1) + f3 (n-1) q
+
+f4:: Int -> Float -> Float
+f4 n q
+    |n == 1 = q+1
+    |otherwise = f2 (2*n) q - f2(n-1) q
+
+-- EJERCICIO 11
+{--
+problema eAprox{n:Z} R {
+requiere:{n >= 0}
+asegura: {res= sumatoria de 1/i! hasta n}
+}
+--}
+
+factorial:: Int -> Int
+factorial n
+    |n == 0 = 1
+    |otherwise = n * (factorial (n-1))
+
+eAprox:: Int -> Float
+eAprox n
+    |n == 0 = 1
+    |otherwise = 1/fromIntegral(factorial(n)) + eAprox(n-1) 
+
+e = eAprox (10)
+
+-- EJERCICIO 12
+{--
+problema sucesion{n:Z} :R {
+requiere: {n > 0}
+asegura: {res = la sucesión an}
+}
+
+problema raizDe2Aprox {m:Z} :R {
+    requiere:{m > 0}
+    asegura: {res = sucesion(m) ~~ sqrt(2)}
+}--} 
+
+sucesion:: Int -> Float
+sucesion n
+    |n == 1 = 2
+    |otherwise = 2 + 1/sucesion(n-1) 
+--    |otherwise = 2 + (1/(sucesion n-1))
+
+raizDe2Aprox:: Int -> Float
+raizDe2Aprox n = sucesion n - 1
+
+-- EJERCICIO 14
+sumaPotencias:: Int -> Int -> Int -> Int
+sumaPotencias q n m
+    |n == 1 && m == 1 = q^2
+--    |otherwise = q^(n + m) + sumaPotencias q n (m-1)
 
 -- EJERCICIO 13
-fDobleSum:: Int -> Int -> Int
-fDobleSum m p
+{-- fDobleSum :: Float -> Float -> Float
+fDobleSum p m
     |p == 1 = f2 m 1
 --    |m == 1 = 0
     |otherwise = f2 p m + f2 p (m-1) 
-
+--}
 -- EJERCICIO 16
 -- menorDivisor:: Int -> Int
 -- menorDivisor n
