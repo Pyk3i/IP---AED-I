544d05ab29ebcc4b4c3bb224d410ff2dadd4af41
Pyk3i <Pyk3i@users.noreply.github.com>
2024-04-28 19:57:18 -0300

    Arreglos (No test)

--- simParcial.hs
+++ simParcial.hs
@@ -1,50 +1,10 @@
-relacionesValidas :: [(String, String)] -> Bool
-relacionesValidas [] = False
-relacionesValidas [(x , y)]
-    |x == y = False
-    |otherwise = True
-relacionesValidas[(a , b) , (c , d)]
-    |fst (a,b) == fst (c,d) && snd (a,b) == snd (c,d) = False
-    |fst (a,b) == snd (c,d) && snd (a,b) == fst (c,d) = False
-    |otherwise = True
+module Parcial where
 
-personas :: [(String, String)] -> [String]
-personas [] = ["nadie"]
-personas [(x , y)]
-    |relacionesValidas [(x , y)] = [x , y]
-personas [(a , b) , (c , d)]
-    |relacionesValidas lista = quitarRepetidos([a,b,c,d]) 
-        where lista = [(a,b) , (c,d)]
-
-quitarRepetidos:: [String] -> [String]
-quitarRepetidos [] = []
-quitarRepetidos (x:xs)
-    |perteneceLista x xs = quitarRepetidos xs
-    |otherwise = x : quitarRepetidos xs
-
-
-perteneceLista :: String -> [String] -> Bool -- Auxiliar para verificar que el nombre está en una lista
-perteneceLista _ [] = False
-perteneceLista x (y:xs)
-    |x == y = True
-    |otherwise = perteneceLista x xs
-
-amigosDe :: String -> [(String, String)] -> [String]
-amigosDe "nadie" [] = ["nadie"]
-amigosDe x [(y , z)]
-    |relacionesValidas [(y,z)] && x == y = [z]
-    |relacionesValidas [(y,z)] && x == z = [y]
-{--amigosDe x [(a,b) , (c,d)]
-    |not perteneceLista x --}
-
-
-personaConMasAmigos :: [(String, String)] -> String
-personaConMasAmigos [] = "yo" 
 relacionesValidas :: [(String, String)] -> Bool
 relacionesValidas [] = True
 relacionesValidas (x:xs)
     |perteneceListadeTupla x xs || perteneceListadeTupla (inversoTupla x) xs = False
-    |otherwise = relacionesValidas xs
+    |otherwise = True
 
 personas :: [(String, String)] -> [String]
 personas [] = []
